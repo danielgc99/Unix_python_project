d9fa6ae512dee5c830f2f38d18ce29780750839b
danielgc99 <s242872@dtu.dk>
2025-04-03 11:36:50 +0200

    readpoints fucntion

--- test_script01.py
+++ test_script01.py
@@ -1,1 +1,20 @@
-#
+#QT clustering project
+
+
+##Libraries
+import sys
+
+infile = sys.argv[1]
+
+##Functions
+#loading data
+def readpoints(infile):
+    point_list = []
+    with open(infile) as file:
+        for line in file:
+            line = line.strip().split()
+            point_list.append((line[0],[float(n) for n in line[1:]]))
+    
+    return(point_list)
+
+print(readpoints(infile))
